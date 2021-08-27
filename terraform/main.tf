fcb7eb5c15005a5321971d61477e873f395deb86
Yvon QUEROMES <yvon.queromes@metanext.com>
2021-08-27 16:32:39 +0200

    com2

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,24 +33,24 @@ provider "azuread" {
 ######################### Resources #####################
 #
 
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-pipeline-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-}
-
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
+#resource "azurerm_virtual_network" "main" {
+#  name                = "${var.prefix}-pipeline-network"
+#  address_space       = ["10.0.0.0/16"]
+#  location            = azurerm_resource_group.main.location
+#  resource_group_name = azurerm_resource_group.main.name
+#}
+#
+#resource "azurerm_subnet" "internal" {
+#  name                 = "internal"
+#  resource_group_name  = azurerm_resource_group.main.name
+#  virtual_network_name = azurerm_virtual_network.main.name
+#  address_prefixes     = ["10.0.2.0/24"]
+#}
 
 resource "azurerm_public_ip" "main" {
   name                = "${var.prefix}-pip"
   location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  resource_group_name =  "${var.resource_group_name}"
   allocation_method   = "Dynamic"
   domain_name_label   = "${var.prefix}-is-testing"
 
@@ -66,7 +66,7 @@ resource "azurerm_network_interface" "main" {
 
   ip_configuration {
     name                          = "testconfiguration1"
-    subnet_id                     = var.subnet_id
+    subnet_id                     = "${var.subnet_id}"
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.main.id
   }
