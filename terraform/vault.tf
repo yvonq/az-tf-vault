5eac7610bfe709518cf1e04d1c8374bf4739c05f
Yvon QUEROMES <yvon.queromes@metanext.com>
2021-08-30 08:48:57 +0200

    gg7

--- terraform/vault.tf
+++ terraform/vault.tf
@@ -103,7 +103,7 @@ resource "azurerm_network_security_rule" "vault_allow_ssh" {
   destination_port_range      = "22"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  location                    = azurerm_resource_group.yqs-tf-demo-rg.location
+  
   resource_group_name         = azurerm_resource_group.yqs-tf-demo-rg.name
   network_security_group_name = azurerm_network_security_group.vault.name
 }
